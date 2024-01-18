211d83d21ddbf3e9550c8625a456575e094be821
owusuvictorjnr <owusuvictor80@gmail.com>
2024-01-18 13:58:48 +0000

    footer

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=" tex-white h-[5rem] shadow-md bg-amber-400">
+      <div className="text-center capitalize">
+        all rights reserved &copy; 2023
+      </div>
+    </div>
+  )
+}
+
+export default Footer
