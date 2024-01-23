c93be499bc48ee19e8a08b67cb9fac4bf80faaf5
owusuvictorjnr <owusuvictor80@gmail.com>
2024-01-23 16:07:55 +0000

    username

--- app/(root)/booking/page.jsx
+++ app/(root)/booking/page.jsx
@@ -1,12 +1,23 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { DateCalendar, TimePicker } from '@mui/x-date-pickers'
+import { useForm } from 'react-hook-form'
 import dayjs from 'dayjs'
 
 export default function Booking() {
   const [selectedDate, setSelectedDate] = useState(null)
   const [selectedTime, setSelectedTime] = useState(null)
+
+  const [name, setName] = useState(null)
+
   const [mounted, setMounted] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    formState: { errors },
+  } = useForm()
 
   useEffect(() => {
     setMounted(true)
@@ -24,6 +35,7 @@ export default function Booking() {
         body: JSON.stringify({
           date: dayjs(setSelectedDate).format('YYYY-MM-DD'),
           time: dayjs(setSelectedTime).format('h:mm A'),
+          name: setName,
         }),
       })
       const data = await response.json()
@@ -46,22 +58,65 @@ export default function Booking() {
               disablePast
               value={selectedDate}
               onChange={(newDate) => setSelectedDate(newDate)}
+              className="font-bold tracking-widest text-center px-5"
             />
           </div>
-          <TimePicker
-            views={['hours', 'minutes']}
-            value={selectedTime}
-            onChange={(newTime) => setSelectedTime(newTime)}
-            className="font-bold tracking-widest text-center"
-          />
+          <div className="px-5">
+            <TimePicker
+              views={['hours', 'minutes']}
+              value={selectedTime}
+              onChange={(newTime) => setSelectedTime(newTime)}
+              className=" w-full md:w-[20rem] px-5"
+            />
+          </div>
         </div>
 
-        <button
-          onClick={handleBooking}
-          className="py-2 px-4 capitalize border w-full bg-green-500 rounded mt-2 font-bold"
-        >
-          <p className="text-center">submit</p>
-        </button>
+        <div className="px-5">
+          <form onClick={handleBooking}>
+            <input
+              type="text"
+              value={name}
+              onChange={handleBooking}
+              placeholder="Full name"
+              className=" border-2 rounded py-2 px-2 w-full mt-5"
+              {...register('name', {
+                required: true,
+                message: 'This field is required',
+              })}
+            />
+
+            <input
+              type="text"
+              value={name}
+              onChange={handleBooking}
+              placeholder="Email"
+              className=" border-2 rounded py-2 px-2 w-full mt-5"
+              {...register('email', {
+                required: true,
+                message: 'This field is required',
+                validated: (v) => /.+@.+\..+/.test(v),
+              })}
+            />
+
+            <textarea
+              type="text"
+              value={name}
+              onChange={handleBooking}
+              placeholder="Leave a message"
+              className=" border-2 rounded py-2 px-2 w-full mt-5 h-[10rem]"
+              {...register('name', {
+                // required: true,
+                // message: 'This field is required',
+              })}
+            />
+          </form>
+          <button
+            onClick={handleBooking}
+            className="py-2 px-4 capitalize border w-full bg-green-500 rounded mt-2 font-bold"
+          >
+            <p className="text-center">submit</p>
+          </button>
+        </div>
       </div>
     </div>
   )
